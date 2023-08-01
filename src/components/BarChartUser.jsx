de30633d8fb8df07f6925f71cba3e03c12f20f05
Cédric <gloucedric@gmail.com>
2023-08-01 13:11:46 +0200

    correction soutenance 2

--- src/components/BarChartUser.jsx
+++ src/components/BarChartUser.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const BarChartUser = ({ dataUserActivity }) => {
+    // taille min du recharts barr 650px 
+    const [widthBarr, setWidthBarr] = useState(650);
+    // taille de l'ecran
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    const funcScreenWidth = () => {
+        setScreenWidth(window.innerWidth);
+    }
+    console.log(screenWidth)
+    useEffect(() => {
+        // si la taile de l'ecran est superieur à
+        if (screenWidth > 1124) {
+            // window.addEventListener("resize", setX(700));
+            // return () => window.removeEventListener("resize", setX);
+            setWidthBarr(700)
+
+        }
+        if (screenWidth > 1224) {
+            // window.addEventListener("resize", );
+            // return () => window.removeEventListener("resize", setX(700));
+            setWidthBarr(750)
+        }
+        if (screenWidth > 1324) {
+            setWidthBarr(800)
+        }
+        if (screenWidth > 1424) {
+            setWidthBarr(850)
+        }
+
+        window.addEventListener("resize", funcScreenWidth);
+        return () => window.removeEventListener("resize", funcScreenWidth);
+
+
+    }, [funcScreenWidth, setWidthBarr])
+
+
+    console.log(screenWidth);
     return (
         <div className='div-barchart'>
             <p>Activité quotidienne</p>
+
             <BarChart
-                width={650}
+                width={widthBarr}
                 height={200}
                 data={dataUserActivity}
                 margin={{
@@ -25,6 +63,7 @@ const BarChartUser = ({ dataUserActivity }) => {
                 <Bar radius={[20, 20, 0, 0]} maxBarSize={10} dataKey="kilogram" fill="#282D30" />
                 <Bar radius={[20, 20, 0, 0]} maxBarSize={10} dataKey="calories" fill="#E60000" />
             </BarChart>
+
         </div>
     );
 };
